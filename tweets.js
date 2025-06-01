03cc1f45c4b94a26eb4fe6fdb13460dfde5d75fc
rummiesz <yarennostale@gmail.com>
2025-06-01 22:35:38 +0300

    Update tweets.js

--- tweets.js
+++ tweets.js
@@ -1,27 +1,52 @@
-const { twitter, treshold, sleep } = require('./config')
+const { twitter, sleep } = require('./config') 
 const { ApiResponseError } = require('twitter-api-v2')
 
 const main = async () => {
   const user = await twitter.v2.me()
-  const tweets = await twitter.v2.userTimeline(user.data.id, { 'tweet.fields': 'created_at' })
+  let nextToken = null
+  let deletedCount = 0
+  const maxDeletions = 3200 
 
-  for await (const tweet of tweets) {
-    try {
-      if (Date.parse(tweet.created_at) <= treshold) {
-        console.log(`Deleting tweet ${tweet.id}`)
+  do {
+    const { data, meta } = await twitter.v2.userTimeline(user.data.id, {
+      'tweet.fields': 'created_at',
+      max_results: 100,
+      pagination_token: nextToken
+    })
 
+    for (const tweet of data) {
+      try {
+        if (deletedCount >= maxDeletions) {
+          console.log('Günlük silme limitine ulaşıldı')
+          return
+        }
+
+        console.log(`Siliniyor: ${tweet.id} (${tweet.created_at})`)
         await twitter.v1.deleteTweet(tweet.id)
-      }
-    } catch (error) {
-      if (error instanceof ApiResponseError && error.rateLimitError && error.rateLimit) {
-        console.log('rate limit hit, waiting for the timer reset (this can take up to 15 minutes)')
-        await sleep(error.rateLimit.reset)
-        continue
-      }
+        deletedCount++
 
-      throw error
+     
+        await sleep(3000) 
+
+      } catch (error) {
+        if (error instanceof ApiResponseError) {
+          if (error.rateLimitError && error.rateLimit) {
+            const waitTime = error.rateLimit.reset * 1000 - Date.now()
+            console.log(`Rate limit! ${Math.round(waitTime/1000)} saniye bekleniyor...`)
+            await sleep(waitTime)
+            continue
+          }
+          console.error(`API Hatası: ${error.code} - ${error.message}`)
+        } else {
+          console.error('Beklenmeyen Hata:', error)
+        }
+      }
     }
-  }
+
+    nextToken = meta?.next_token
+  } while (nextToken && deletedCount < maxDeletions)
+
+  console.log(`Toplam ${deletedCount} tweet silindi!`)
 }
 
-main().catch((e) => console.log(e))
+main().catch((e) => console.error('Hata:', e))

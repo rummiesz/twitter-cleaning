cdc82b972ce2aeacbda0dfc81c697a9347edf896
rummiesz <yarennostale@gmail.com>
2025-06-01 23:20:17 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 const { TwitterApi } = require('twitter-api-v2');
 require('dotenv').config();
 
-// Debug için anahtar kontrolü
-console.log('Consumer Key:', process.env.CONSUMER_KEY ? '***' + process.env.CONSUMER_KEY.slice(-4) : 'MISSING');
+// DEBUG: Anahtarları kontrol edelim
+console.log('Consumer Key:', process.env.CONSUMER_KEY?.slice(0, 4) + '...');
 
 const client = new TwitterApi({
   appKey: process.env.CONSUMER_KEY,
@@ -10,3 +10,12 @@ const client = new TwitterApi({
   accessToken: process.env.ACCESS_TOKEN,
   accessTokenSecret: process.env.ACCESS_TOKEN_SECRET,
 });
+
+// Client'ı test edelim
+client.v2.me().then(user => {
+  console.log('API Bağlantısı Başarılı! User ID:', user.data.id);
+}).catch(err => {
+  console.error('API Bağlantı Hatası:', err);
+});
+
+module.exports = { twitter: client };

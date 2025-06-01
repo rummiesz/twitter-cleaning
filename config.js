c3b5c3f32d345d0f97fb610951578d0f1380cf14
rummiesz <yarennostale@gmail.com>
2025-06-01 23:01:26 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,12 @@
-require('dotenv').config()
-const { TwitterApi } = require('twitter-api-v2')
+const { TwitterApi } = require('twitter-api-v2');
+require('dotenv').config();
 
-module.exports.twitter = new TwitterApi({
-  appKey: process.env.TWITTER_KEY,
-  appSecret: process.env.TWITTER_SECRET,
-  accessToken: process.env.TWITTER_ACCESS_TOKEN_KEY,
-  accessSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
-})
+// Debug için anahtar kontrolü
+console.log('Consumer Key:', process.env.CONSUMER_KEY ? '***' + process.env.CONSUMER_KEY.slice(-4) : 'MISSING');
 
-module.exports.treshold = Date.now() - 86400000 * process.env.DELETE_AFTER_DAYS || 14
-
-module.exports.sleep = (reset) => new Promise((r) => setTimeout(r, reset * 1000 - Date.now()))
+const client = new TwitterApi({
+  appKey: process.env.CONSUMER_KEY,
+  appSecret: process.env.CONSUMER_SECRET,
+  accessToken: process.env.ACCESS_TOKEN,
+  accessTokenSecret: process.env.ACCESS_TOKEN_SECRET,
+});
